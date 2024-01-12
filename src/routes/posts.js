1b732d75204c7eac30732c7c14ae4a39e2a65529
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2024-01-12 17:02:11 +0200

    handling middleware issues

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,16 @@
-
-import  Router  from 'express';
+import Router from "express";
 const router = Router();
-import {upload,create ,findAll,findOne,updatep,deletep} from '../controllers/post.controllers.js';
-router.post('/create',upload.single("photo"),create);
-router.get('/', findAll);
-router.get('/:id',findOne);
-router.put('/:id',upload.single("photo"), updatep);
-router.delete('/:id', deletep);
+import {
+  upload,
+  create,
+  findAll,
+  findOne,
+  updatep,
+  deletep,
+} from "../controllers/post.controllers.js";
+router.post("/create", upload.single("photo"), create);
+router.get("/", findAll);
+router.get("/:id", findOne);
+router.put("/:id", updatep);
+router.delete("/:id", deletep);
 export default router;
